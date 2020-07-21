4f9e50e696a1c5083ca11fde0956c1b6f52d2f60
LWG1SZH <691467078@qq.com>
2020-07-21 10:40:21 +0800

    commit for rabbit fanout

---
+++ rabbitmq/rabbitmq-direct/rabbit-mq-ack-direct-provider/src/main/java/rabbitmq/messaging/Sender.java
@@ -0,0 +1,29 @@
+package rabbitmq.messaging;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class Sender {
+
+    @Autowired
+    private AmqpTemplate rabbitmqTemplate;
+    @Value("${mq.config.exchange}")
+    private String exchange;
+
+    public void sendErr() throws InterruptedException {
+        String msg = "hello" + new Date();
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(2000);
+            this.rabbitmqTemplate.convertAndSend(exchange, "log.info.routing.key", msg);
+        }
+    }
+    public void send(int n) {
+            String msg = "hello" + n;
+            System.out.println(msg);
+            this.rabbitmqTemplate.convertAndSend(exchange, "log.info.routing.key", msg);
+        }
+}
---
+++ rabbitmq/rabbitmq-direct/rabbit-mq-ack-direct-provider/src/test/java/RabbitmqApplicationTests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import rabbitmq.RabbitmqApplication;
+import rabbitmq.messaging.Sender;
+
+
+@SpringBootTest(classes = RabbitmqApplication.class)
+class RabbitmqApplicationTests {
+
+    @Autowired
+    private Sender sender;
+    @Test
+    public void  send() throws InterruptedException {
+            sender.send(1);
+            Thread.sleep(2000);
+
+
+    }
+
+}
