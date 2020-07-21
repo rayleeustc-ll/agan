e423571452a97e8eb53274c0d9da76aecae1ee02
LWG1SZH <691467078@qq.com>
2020-07-21 10:39:58 +0800

    commit for rabbit fanout

---
+++ rabbitmq/rabbitmq-direct/rabbit-mq-ack-direct.consumer/src/main/java/rabbitmq/messaging/ErrorReciever.java
@@ -0,0 +1,22 @@
+package rabbitmq.messaging;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(bindings = @QueueBinding(
+        value= @Queue(value ="${mq.config.queue.error}",
+                autoDelete = "true"),
+        exchange = @Exchange(value ="${mq.config.exchange}",
+            type = ExchangeTypes.DIRECT),
+        key = "${mq.config.queue.error.routing.key}"
+))
+public class ErrorReciever {
+
+    @RabbitHandler
+    public void process(String msg){
+        System.out.println("error reciever "+ msg);
+        throw new RuntimeException();
+    }
+}
---
+++ rabbitmq/rabbitmq-direct/rabbit-mq-ack-direct.consumer/src/main/java/rabbitmq/messaging/InfoReciever.java
@@ -0,0 +1,22 @@
+package rabbitmq.messaging;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(bindings = @QueueBinding(
+        value= @Queue(value ="${mq.config.queue.info}",
+                autoDelete = "true"),
+        exchange = @Exchange(value ="${mq.config.exchange}",
+            type = ExchangeTypes.DIRECT),
+        key = "${mq.config.queue.info.routing.key}"
+))
+public class InfoReciever {
+
+    @RabbitHandler
+    public void process(String msg){
+        System.out.println("Info reciever "+ msg);
+        throw new RuntimeException();
+    }
+}
